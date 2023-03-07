2ee77d272fa11ddeb57bf371cb2956e7b939eaeb
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-03-07 16:21:12 +0900

    [Bronze V] Title: 사분면 고르기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.swift
@@ -0,0 +1,15 @@
+let x = Int(readLine()!)!
+let y = Int(readLine()!)!
+
+if x>0 && y>0 {
+    print("1")
+}
+else if x>0 && y<0 {
+    print("4")
+}
+else if x<0 && y<0 {
+    print("3")
+}
+else {
+    print("2")
+}
