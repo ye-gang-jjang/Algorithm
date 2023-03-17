336b8c367f4eb334c35a2c93280ba018eccbdb29
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-03-17 20:03:46 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.13 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.swift
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.swift
@@ -2,6 +2,5 @@ import Foundation
 
 func solution(_ numbers:[Int]) -> Int {
     let sortArr = numbers.sorted(by: >)
-    let arrMult = sortArr[0] * sortArr[1]
-    return arrMult
+    return sortArr[0] * sortArr[1]
 }
