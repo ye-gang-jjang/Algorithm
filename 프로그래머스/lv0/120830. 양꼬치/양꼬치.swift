cf8eccbd45f090a8f688e8a69dcbad968c1aa62d
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-03-17 18:00:42 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.swift
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.swift
@@ -1,12 +1,7 @@
 import Foundation
 
-func solution(_ n:Int, _ k:Int) -> Int {
-    let nPrice = n * 12000
-    let sale =  n / 10
-    let kPrice = (k-sale) * 2000
-    
-    
-    var result = nPrice + kPrice
+func solution(_ n:Int, _ k:Int) -> Int {    
+    var result = n * 12000 + (k-(n/10)) * 2000
     
     return result
 }
