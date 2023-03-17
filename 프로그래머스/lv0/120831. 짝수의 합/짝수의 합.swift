5040937b03e2a4aec1197c2333372b33a30e1e77
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-03-17 16:51:55 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var sum = 0
+    
+    for i in 1...n {
+        if i % 2 == 0 {
+            sum += i
+        }
+    }
+    
+    return sum
+}
