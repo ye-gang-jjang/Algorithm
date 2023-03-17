424bf1626c5908c924eedf67acaccb190b8107f3
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-03-17 16:45:45 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ angle:Int) -> Int {    
+    if angle == 180 {
+        return 4
+    } else if angle > 90 {
+        return 3
+    } else if angle == 90 {
+        return 2
+    } else {
+        return 1
+    }
+}
