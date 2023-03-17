10213212f850e9a8467a0133d579908ade787a01
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-03-17 20:30:22 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ dot:[Int]) -> Int {
+    if dot[0] > 0 {
+      if dot[1] > 0 {
+             return 1
+         } else {
+             return 4
+         }
+    } else {
+        if dot[1] > 0 {
+            return 2
+        } else {
+            return 3
+        }
+    }
+}
