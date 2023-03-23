ec41f5cb98e16a6d84f4d6567ee4fa6b7599d823
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-03-23 20:59:55 +0900

    Delete 프로그래머스/lv0/120834. 외계행성의 나이 directory

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-func solution(_ age:Int) -> String {
-    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-    return String(age).map{alphabet[Int(String($0))!]}.joined()
-}
