479eeaeadca574e788174759ab1421f8ab8994ad
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-03-26 14:30:35 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.31 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ my_string:String) -> String {
+    let strLowe = my_string.lowercased()
+    let strUpper = my_string.uppercased()
+    
+    var result = ""
+    
+    for i in my_string.indices {
+        if my_string[i] != strLowe[i] {
+            result += String(strLowe[i])
+        } else {
+            result += String(strUpper[i])
+        }
+    }
+    
+    return result
+}
