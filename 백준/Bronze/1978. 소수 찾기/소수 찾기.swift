e398167bd74933588ce706ae4e16437c996ec17d
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-04-05 22:15:00 +0900

    [Bronze II] Title: 소수 찾기, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.swift
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.swift
@@ -1,20 +1,8 @@
 import Foundation
 
 let N = Int(readLine()!)!
-var count = 0
 let input = readLine()!.split(separator: " ").map{Int($0)!}
 
-for i in 0 ..< N {
-    if input[i] == 1 {
-        count += 1
-        continue
-    }
-    
-    for j in 2 ..< input[i] {
-        if input[i] % j == 0 {
-            count += 1
-            break
-        }
-    }
-}
-print(input.count - count)
+let result = input.filter{(i:Int)in i != 1 && Array(2..<i).filter{i%$0==0}.count==0}.count
+
+print(result)
