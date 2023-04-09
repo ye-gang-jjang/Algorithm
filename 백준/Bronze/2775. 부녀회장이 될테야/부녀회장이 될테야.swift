238461ea9f6c0ab97e4318be6f6abd42d0f66b66
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-04-09 14:44:37 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.swift
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.swift
@@ -1,17 +1,18 @@
 let T = Int(readLine()!)!
 var aptArr = Array(repeating: Array(repeating: 1, count: 15), count: 15)
 
+for i in 1..<15 {
+    aptArr[0][i] = i
+}
+
+for i in 1..<15 {
+    for j in 2..<15 {
+        aptArr[i][j] = aptArr[i][j-1] + aptArr[i-1][j]
+    }
+}
+
 for _ in 0..<T {
     let k = Int(readLine()!)!
     let n = Int(readLine()!)!
-    
-    for idx in 1...n {
-        aptArr[0][idx] = idx + 1
-    }
-    for idxK in 1...k {
-        for idxN in 1...n {
-            aptArr[idxK][idxN] = aptArr[idxK][idxN-1] + aptArr[idxK-1][idxN]
-        }
-    }
-    print(aptArr[k][n-1])
+    print(aptArr[k][n])
 }
