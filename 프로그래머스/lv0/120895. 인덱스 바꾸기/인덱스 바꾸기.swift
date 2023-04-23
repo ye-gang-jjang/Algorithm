02bc2d0423d2e8d23e68c60d4125206b6e342bed
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-04-23 19:48:33 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.swift
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.swift
@@ -2,14 +2,8 @@ import Foundation
 
 func solution(_ my_string:String, _ num1:Int, _ num2:Int) -> String {
     var str = Array(my_string)
-    var temp1 = str[num1]
-    var temp2 = str[num2]
     
-    temp1 = str[num1]
-    temp2 = str[num2]
-    
-    str[num1] = temp2
-    str[num2] = temp1
+    str.swapAt(num1, num2)
     
     return String(str)
 }
