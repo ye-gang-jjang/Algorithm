a97485c096d2b47ae75f87a95a6edacdca93125d
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-04-28 18:03:14 +0900

    [level 0] Title: 옹알이 (1), Time: 0.12 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.swift
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.swift
@@ -1,13 +1,7 @@
 import Foundation
 
 func solution(_ babbling:[String]) -> Int {
-    
-    var babblingCnt = [Int]()
-    
-    for i in babbling.indices {
-        babblingCnt.append(babbling[i].count)
-    }
-    
+    let babblingCnt: [Int] = babbling.map{ $0.count }
     let list = ["aya": 3, "ye": 2, "woo": 3, "ma": 2]
     var result = 0
     
