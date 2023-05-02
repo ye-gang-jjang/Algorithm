bbe6d65f14078e99abbbcb676fd3b9538510d448
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-05-02 19:53:30 +0900

    [level 0] Title: 종이 자르기, Time: 0.00 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.swift
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ M:Int, _ N:Int) -> Int {
-    if (M == 1) && (N == 1) {
-        return 0
-    } else {
-        return (M*N) - 1
-    }
+    return M * N - 1
 }
