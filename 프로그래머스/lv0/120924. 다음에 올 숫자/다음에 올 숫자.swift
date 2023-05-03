41787b0f13909c4734c439f31dcaebbb3512fdab
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-05-03 12:11:10 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ common:[Int]) -> Int {
+    if common[1] - common[0] == common[2] - common[1] {
+        return common.last! + (common[1] - common[0])
+    } else {
+        return common.last! * (common[1] / common[0])
+    }
+}
