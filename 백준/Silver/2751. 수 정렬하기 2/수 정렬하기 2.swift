b3b5bde156fdc484ae7f4940fa815acf41d665c1
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-05-12 18:48:38 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 692 ms, Memory: 86808 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.swift
@@ -0,0 +1,17 @@
+let N = Int(readLine()!)!
+
+var arr = Array(repeating: false, count: 2000001)
+var str = ""
+
+for _ in 1...N {
+    let num = Int(readLine()!)!
+    arr[num + 1000000] = true
+}
+
+for i in 0..<2000001 {
+    if arr[i] == true {
+        str += "\(i-1000000)\n"
+    }
+}
+
+print(str)
