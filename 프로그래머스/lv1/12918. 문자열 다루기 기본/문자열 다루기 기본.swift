3fc887b808dcd9ff8279e8484c3954be0626b1de
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-05-19 20:18:34 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
@@ -1,12 +1,3 @@
 func solution(_ s:String) -> Bool {
-    if s.count == 4 || s.count == 6 {
-        for i in s {
-            if !i.isNumber {
-                return false
-            }
-        }
-        return true
-    } else {
-        return false
-    }
+    return true
 }
