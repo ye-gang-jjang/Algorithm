cb31880134458ce5d392acf4515e45c307592322
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-05-22 20:15:25 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 228 ms, Memory: 72240 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.swift
@@ -0,0 +1,16 @@
+let N = Int(readLine()!)!
+var arr = [(Int, Int)]()
+
+for _ in 0..<N {
+    let input = readLine()!.split(separator: " ").map{ Int($0)! }
+    arr.append((input[0], input[1]))
+}
+
+// 1 1 / 1 2
+arr.sort(by: {
+    $0.1 == $1.1 ? $0.0 < $1.0 : $0.1 < $1.1
+})
+
+for i in 0..<N {
+    print("\(arr[i].0) \(arr[i].1)")
+}
