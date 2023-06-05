c0485d15fe52920360d27a5907a2a6d574b8af70
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-06-06 04:02:20 +0900

    [Silver III] Title: 통계학, Time: 304 ms, Memory: 86120 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.swift
+++ 백준/Silver/2108. 통계학/통계학.swift
@@ -1,35 +1,29 @@
 import Foundation
-let n = Int(readLine()!)!
-var arr: [Int] = []
-var average: Double = 0.0
-var sum = 0
-var dict = [Int:Int]()
 
-for _ in 0..<n {
+let N = Int(readLine()!)!
+var arr = [Int]()
+var dict = [Int: Int]()
+
+
+for _ in 0..<N {
     let num = Int(readLine()!)!
     arr.append(num)
     dict[num, default: 0] += 1
 }
 
-//산술평균
-for i in 0..<arr.count {
-    sum += arr[i]
-}
-average = (Double(sum) / Double(arr.count))
-print("\(Int(round(average)))")
+// 산술 평균
+let sum = arr.reduce(0, +)
+let avg = Double(sum) / Double(N)
+print(Int(round(avg)))
 
-//중앙값
+// 중앙값
 arr.sort()
-print("\(arr[((arr.count / 2))])")
+print(arr[N/2])
 
-//최빈값
+// 최빈값
 let maxValue = dict.max(by: {$0.value < $1.value})!.value
 var mode = dict.filter({ $0.value == maxValue}).keys.sorted()
-if mode.count > 1 {
-    print("\(mode[1])")
-}else {
-    print("\(mode[0])")
-}
+print(mode.count > 1 ? mode[1] : mode[0])
 
 //범위
 print("\(arr.max()! - arr.min()!)")
