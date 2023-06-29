1778615f7518cc8b205be28bddfde91fcd17faf1
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-06-30 02:18:30 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 268 ms, Memory: 76200 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.swift
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.swift
@@ -1,11 +1,4 @@
-let NM = readLine()!.split(separator: " ").map{ Int($0)! }
-var arr = [0] + readLine()!.split(separator: " ").map { Int($0)! }
-
-for i in 1...NM[0] {
-    arr[i] += arr[i - 1]
-}
-
-for _ in 0..<NM[1] {
-    let input = readLine()!.split(separator: " ").map{ Int($0)! }
-    print(arr[input[1]] - arr[input[0] - 1])
-}
+let NM=readLine()!.split(separator: " ").map{Int($0)!}
+var arr=[0]+readLine()!.split(separator: " ").map {Int($0)!}
+for i in 1...NM[0]{arr[i]+=arr[i-1]}
+for _ in 0..<NM[1]{let ij=readLine()!.split(separator: " ").map{Int($0)!};print(arr[ij[1]]-arr[ij[0]-1])}
