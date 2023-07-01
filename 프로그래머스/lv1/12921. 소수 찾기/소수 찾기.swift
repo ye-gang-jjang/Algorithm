831ee3981c30d52697b4802b8013c4dbe2d6d6e7
Yegang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-07-01 22:15:49 +0900

    [level 1] Title: 소수 찾기, Time: 1191.50 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var isPrime = true
+    var count = 0
+    
+    for i in 2...n {
+        isPrime = true
+        
+        for j in 2...Int((sqrt(Double(n)))) + 1 {
+            if i != j && i % j == 0 {
+                isPrime = false
+                break
+            }
+        }
+        
+        count = isPrime ? count + 1 : count
+    }
+    return count
+}
