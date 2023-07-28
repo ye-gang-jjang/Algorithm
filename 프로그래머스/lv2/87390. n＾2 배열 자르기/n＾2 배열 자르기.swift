c07129f3377b18b03133e7214217e08f3e404409
ye-gang-jjang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-07-28 19:09:54 +0900

    [level 2] Title: n^2 배열 자르기, Time: 30.60 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
+    var result = [Int]()
+    
+    result = (Int(left)...Int(right)).map {
+        max($0 / n + 1, $0 % n + 1)
+    }
+    return result
+}
