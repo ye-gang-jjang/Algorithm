e2e47bf3c32fb124357dec9f5275ce1cfcdfd975
ye-gang-jjang <81469446+ye-gang-jjang@users.noreply.github.com>
2023-09-04 13:37:35 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+let input = readLine()!
+var alphabet = Array(repeating: 0, count: 26)
+var isPalindrome = true
+
+for char in input {
+    alphabet[Int(char.asciiValue!)-65] += 1
+}
+
+if input.count % 2 == 0 {
+    var front = ""
+    var back = ""
+    
+    for i in 0..<26 {
+        if alphabet[i] % 2 != 0 {
+            isPalindrome = false
+            break
+        }
+        
+        if alphabet[i] != 0 {
+            for _ in 0..<alphabet[i]/2 {
+                front += String(Character(UnicodeScalar(i+65)!))
+                back += String(Character(UnicodeScalar(i+65)!))
+            }
+        }
+    }
+    front += back.reversed()
+    
+    print(isPalindrome ? front : "I'm Sorry Hansoo")
+    
+} else {
+    var front = ""
+    var back = ""
+    var mid = ""
+    
+    for i in 0..<26 {
+        if alphabet[i] % 2 != 0 {
+            if mid == "" {
+                mid += String(Character(UnicodeScalar(i+65)!))
+            } else {
+                isPalindrome = false
+                break
+            }
+        }
+        
+        if alphabet[i] != 0 {
+            for _ in 0..<alphabet[i]/2 {
+                front += String(Character(UnicodeScalar(i+65)!))
+                back += String(Character(UnicodeScalar(i+65)!))
+            }
+        }
+    }
+    
+    front += mid
+    front += back.reversed()
+    print(isPalindrome ? front : "I'm Sorry Hansoo")
+}
