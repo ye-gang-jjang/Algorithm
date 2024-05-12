92411ad7e04e485331c72a3efc68029be55f7bc3
ye-gang <81469446+ye-gang-jjang@users.noreply.github.com>
2024-05-13 02:05:01 +0900

    [Silver V] Title: 단어 정렬, Time: 268 ms, Memory: 74924 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.swift
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.swift
@@ -2,13 +2,6 @@ let N = Int(readLine()!)!
 var arr = [String]()
 var setArr = Set<String>()
 
-for _ in 0..<N {
-    let input = readLine()!
-    setArr.insert(input)
-}
-
+for _ in 0..<N { setArr.insert(readLine()!) }
 arr = Array(setArr).sorted(by: {$0 < $1}).sorted(by: {$0.count < $1.count})
-
-for i in arr {
-    print(i)
-}
+print(arr.map{ String($0) }.joined(separator: "\n"))
