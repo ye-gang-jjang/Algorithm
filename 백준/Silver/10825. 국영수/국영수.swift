db0a5c42a627db7968ed891d3d9f97b4ccc022c8
ye-gang <81469446+ye-gang-jjang@users.noreply.github.com>
2024-05-13 19:29:33 +0900

    [Silver IV] Title: 국영수, Time: 212 ms, Memory: 78860 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.swift
+++ 백준/Silver/10825. 국영수/국영수.swift
@@ -1,29 +1,21 @@
-import Foundation
-
-struct Info {
-    let name: String
-    let korean: Int
-    let english: Int
-    let math: Int
-}
-
 let N = Int(readLine()!)!
-var students = [Info]()
+var arr = [(String, Int, Int, Int)]()
 
 for _ in 0..<N {
     let input = readLine()!.split(separator: " ")
-    students.append(Info(name: String(input[0]), korean: Int(input[1])!, english: Int(input[2])!, math: Int(input[3])!))
+    arr.append(
+        (String(input[0]), Int(input[1])!, Int(input[2])!, Int(input[3])!)
+    )
 }
 
-students = students.sorted(){ $0.korean == $1.korean ? // 국어 점수가 같다면
-    ($0.english == $1.english ? // 영어 점수가 같다면
-        ($0.math == $1.math ? // 수학 점수가 같다면
-        $0.name < $1.name: // 이름을 오름차순으로
-        $0.math > $1.math) // 영어 점수가 같고, 수학 점수가 다르다면 수학 점수를 오름차순으로
-            : $0.english < $1.english) // 국어 점수가 같고, 영어점수가 다르다면 내림차순으로
-    : $0.korean > $1.korean }
-
-//print(students.map{ String($0.name) }.split(separator: "\n"))
-for s in students {
-    print(s.name)
+arr.sort {
+    $0.1 == $1.1 ?
+    $0.2 == $1.2 ?
+    $0.3 == $1.3 ?
+    $0.0 < $1.0 :
+    $0.3 > $1.3 :
+    $0.2 < $1.2 :
+    $0.1 > $1.1
 }
+
+print(arr.map{ String($0.0) }.joined(separator: "\n"))
